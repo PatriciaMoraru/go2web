5f83cf5ab2f2398cff60654616739c8d068c4dc0
Patricia Moraru <patriciamoraruv@gmail.com>
2026-04-06 10:21:14 +0300

    feat: add HttpResponse data model

---
+++ go2web/HttpResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace go2web;
+
+public class HttpResponse
+{
+    public int StatusCode { get; set; }
+    public string StatusMessage { get; set; } = "";
+    public Dictionary<string, string> Headers { get; set; } = new();
+    public string Body { get; set; } = "";
+    public string RawResponse { get; set; } = "";
+}
